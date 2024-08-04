b394b208a4c2991cb972e370b4d2eb9f76d0aa7c
leehyeonhee <2hyeonheedev@gmail.com>
2024-08-04 16:26:05 +0900

    feat: 가계부에 사용자 초대하는 API 생성

--- src/main/java/com/kb/goaler/member_account_book/controller/MemberAccountBookController.java
+++ src/main/java/com/kb/goaler/member_account_book/controller/MemberAccountBookController.java
@@ -1,6 +1,7 @@
 package com.kb.goaler.member_account_book.controller;
 
 import com.kb.goaler.global.dto.ApiResponse;
+import com.kb.goaler.member.dto.MemberInfoRequest;
 import com.kb.goaler.member.dto.MemberInfoResponse;
 import com.kb.goaler.member_account_book.service.MemberAccountBookService;
 import lombok.RequiredArgsConstructor;
@@ -20,5 +21,13 @@ public class MemberAccountBookController {
     public ResponseEntity<ApiResponse<List<MemberInfoResponse>>> getMembers(@PathVariable Long accountBookIdx) {
         List<MemberInfoResponse> responses = memberAccountBookService.getMemberInfoList(accountBookIdx);
         return ResponseEntity.ok().body(ApiResponse.success(responses));
+    }
+
+    @PostMapping("/{accountBookIdx}/users")
+    public ResponseEntity<ApiResponse<Void>> inviteMembers(@PathVariable Long accountBookIdx,
+                                                           @RequestBody MemberInfoRequest request) {
+
+        memberAccountBookService.inviteMember(accountBookIdx, request);
+        return ResponseEntity.ok(ApiResponse.success(null));
     }
 }
--- src/main/java/com/kb/goaler/member_account_book/repository/MemberAccountBookRepository.java
+++ src/main/java/com/kb/goaler/member_account_book/repository/MemberAccountBookRepository.java
@@ -1,15 +1,20 @@
 package com.kb.goaler.member_account_book.repository;
 
 
+import com.kb.goaler.account_book.entity.AccountBookEntity;
+import com.kb.goaler.member.entity.MemberEntity;
 import com.kb.goaler.member_account_book.entity.MemberAccountBookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface MemberAccountBookRepository extends JpaRepository<MemberAccountBookEntity, Long> {
 
     List<MemberAccountBookEntity> findAllByAccountBookIdx(Long accountBookIdx);
+
+    Optional<MemberAccountBookEntity> findByAccountBookAndMember(AccountBookEntity accountBook, MemberEntity member);
 }
