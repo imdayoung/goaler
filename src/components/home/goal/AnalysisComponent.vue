5d68fed2ee83dfe395829d701fc8d470610c0e81
2da00 <86220363+imdayoung@users.noreply.github.com>
2024-08-06 17:51:56 +0900

    design: analysis component 디자인 수정

--- src/components/home/goal/AnalysisComponent.vue
+++ src/components/home/goal/AnalysisComponent.vue
@@ -9,20 +9,23 @@ const splittedText = text.value.split(/[!?.]/);
 <template>
 <div class="card">
   <div class="analysis-title my-3 mx-3">📢AI 금융 비서 비비가 알려드려요! </div>
-  <div class="analysis">
     <div class="speech-bubble mx-5 my-3 px-3 py-3">
       <div v-for="text in splittedText" class="text-white">
         {{ text }}
       </div>
     </div>
-    <div class="img-container px-3">
-      <img src="/src/assets/bibi3.png" class="mx-3"/>
+    <div class="img-container mt-3">
+      <img src="/src/assets/bibi3.png"/>
     </div>
-  </div>
 </div>
 </template>
 
 <style scoped>
+.card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 .text-white {
   color: white;
 }
@@ -30,42 +33,39 @@ const splittedText = text.value.split(/[!?.]/);
   font-family: 'jalnan';
   font-weight: bold;
   text-align: center;
-  font-size: 1.8rem;
+  font-size: 1.6rem;
 }
-.analysis {
-  display: flex;
-  flex-direction: row;
-  position: relative;
-}
+
 .speech-bubble {
-	position: relative;
-	background: #60584c;
-	border-radius: 1em;
-  width: 70%;
+  position: relative;
+  width: 80%;
+  height: 120px;
+  padding: 0px;
+  background: #60584C;
+  -webkit-border-radius: 20px;
+  -moz-border-radius: 20px;
+  border-radius: 20px;
 }
 .speech-bubble:after {
-	content: '';
-	position: absolute;
-	right: 0;
-	top: 70%;
-	width: 0;
-	height: 0;
-	border: 27px solid transparent;
-	border-left-color: #60584c;
-	border-right: 0;
-	border-bottom: 0;
-	margin-top: -13.5px;
-	margin-right: -27px;
+  content: '';
+  position: absolute;
+  border-style: solid;
+  border-width: 20px 20px 0;
+  border-color: #60584C transparent;
+  display: block;
+  width: 0;
+  z-index: 1;
+  bottom: -20px;
+  left: 50%;
 }
 .img-container {
   position: relative;
-  width: 30%;
+  width: 25%;
   display: flex;
   justify-content: flex-end;
   align-items: flex-end;
 }
 img {
-  max-width: 60%;
-  max-height: 60%;
+  width: 100%;
 }
 </style>
